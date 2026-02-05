934fd479c8f4f214de21e8885da026b8b2f02883
kgacandole <kevin.candole@gmail.com>
2026-02-05 18:29:36 +0800

    Create kube_cluster.tf

---
+++ terraform/kube_cluster.tf
@@ -0,0 +1,43 @@
+resource "google_service_account" "gcp_sa" {
+  account_id    = var.google_svc_account_id
+  display_name  = var.google_svc_account_name
+}
+
+resource "google_project_iam_member" "gcp_sa_role" {
+  project = var.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.gcp_sa.email}"
+}
+
+resource "google_container_cluster" "primary" {
+  name                      = var.kube_cluster_name
+  location                  = var.zone_name
+  node_locations = [
+    var.zone_name
+  ]
+  deletion_protection       = false
+  remove_default_node_pool  = true
+  initial_node_count        = var.kube_cluster_node_count
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = var.kube_cluster_node_pool_name
+  location   = var.region_name
+  cluster    = google_container_cluster.primary.name
+  node_count = var.kube_cluster_node_count
+
+  node_config {
+    preemptible  = true
+    machine_type = var.kube_cluster_machine_type
+    service_account = google_service_account.gcp_sa.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  upgrade_settings {
+    strategy = "SURGE"
+    max_surge = 1
+    max_unavailable = 0
+  }
+}
