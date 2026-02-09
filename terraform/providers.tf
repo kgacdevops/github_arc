ad38d78590ff8c9c03add847f43d5d39c1d085ef
Kevin Candole <kevin.candole@gmail.com>
2026-02-09 14:59:43 +0800

    added workfload params

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,5 +10,7 @@ terraform {
 provider "google" {
     project = var.project_id
     region = var.region_name
+    workload_identity_pool = "projects/390209572226/locations/global/workloadIdentityPools/github-pool"
+    workload_identity_provider = "github-provider"
     impersonate_service_account = "terraform-gcp-sa@project-235e2136-7c2e-4409-bad.iam.gserviceaccount.com"
 }
