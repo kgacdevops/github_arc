0196bde114a71db4a8a29cbde00bf406660a8611
kgacandole <kevin.candole@gmail.com>
2026-02-12 17:35:28 +0800

    removed vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,7 +8,5 @@ variable "svc_ip_range" {}
 variable "svc_account_mail" {}
 variable "google_svc_account_id" {}
 variable "google_svc_account_name" {}
-variable "kube_cluster_name" {}
 variable "kube_cluster_node_count" {}
-variable "kube_cluster_node_pool_name" {}
 variable "kube_cluster_machine_type" {}
