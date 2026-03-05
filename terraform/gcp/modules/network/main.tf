bb2e4706a8d4920f9876897c2b849662a655d429
kgacandole <kevin.candole@gmail.com>
2026-03-05 17:00:57 +0800

    increased min ports per vm

--- terraform/gcp/modules/network/main.tf
+++ terraform/gcp/modules/network/main.tf
@@ -44,6 +44,7 @@ resource "google_compute_router_nat" "arc_vpc_nat" {
   source_subnetwork_ip_ranges_to_nat  = "ALL_SUBNETWORKS_ALL_IP_RANGES"
   nat_ip_allocate_option              = "AUTO_ONLY"
   project                             = var.project_id
+  min_ports_per_vm                    = 64
 }
 
 resource "google_compute_firewall" "allow_egress_https" {
