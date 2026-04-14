2d0f9935e1bcd5f550dab30a61db79597d5fff51
kgacandole <kevin.candole@gmail.com>
2026-04-14 16:09:31 +0800

    added cluster block

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -2,3 +2,20 @@ resource "azurerm_resource_group" "arc_rg" {
   name     = "${var.prefix}-aks-rg"
   location = var.location
 }
+
+resource "azurerm_kubernetes_cluster" "arc_cluster" {
+  name                  = "${var.prefix}-cluster"
+  location              = azurerm_resource_group.arc_rg.location
+  resource_group_name   = azurerm_resource_group.arc_rg.name
+  dns_prefix            = "${var.prefix}-dns"
+
+  default_node_pool {
+    name = "${var.prefix}-node-pool"
+    node_count = 1
+    vm_size = "Standard_D2_V2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
