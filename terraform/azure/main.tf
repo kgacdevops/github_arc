0e5eeb6b9b70ff889f0b3843e2f2adf14705eb81
kgacandole <kevin.candole@gmail.com>
2026-04-14 16:15:49 +0800

    changed cluster to private

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -4,10 +4,10 @@ resource "azurerm_resource_group" "arc_rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "arc_cluster" {
-  name                  = "${var.prefix}-cluster"
-  location              = azurerm_resource_group.arc_rg.location
-  resource_group_name   = azurerm_resource_group.arc_rg.name
-  dns_prefix            = "${var.prefix}-dns"
+  name                    = "${var.prefix}-cluster"
+  location                = azurerm_resource_group.arc_rg.location
+  resource_group_name     = azurerm_resource_group.arc_rg.name
+  private_cluster_enabled = true
 
   default_node_pool {
     name = "${var.prefix}nodepool"
